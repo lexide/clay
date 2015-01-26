169982b0ede249a2ab065bf1ef46a993b04ccb0f
Danny Smart <dannysmart@silktide.com>
2015-01-26 17:49:31 +0000

    set up phpUnit

---
+++ test/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Silktide Nibbler. Copyright 2013-2014 Silktide Ltd. All Rights Reserved.
+ */
+
+$autoloader = realpath(dirname(__FILE__) . '/..') . '/vendor/autoload.php';
+
+require_once($autoloader);
