c259fcdc1b33fa776f361ad4ab0b8b616f28218e
Danny Smart <dannysmart@silktide.com>
2017-03-21 17:12:24 +0000

    Add the array key conversion functionality to the NameConverterTrait

--- src/Model/NameConverterTrait.php
+++ src/Model/NameConverterTrait.php
@@ -14,8 +14,8 @@ trait NameConverterTrait
             " ",
             "",
             ucwords( // uppercase the 1st letter of each word
-                str_replace( // replace underscores with spaces
-                    "_",
+                preg_replace( // replace underscores with spaces
+                    "/[^A-Za-z0-9]/",
                     " ",
                     $string
                 )
@@ -30,6 +30,53 @@ trait NameConverterTrait
      */
     private function toSplitCase($string, $separator = "_")
     {
-        return strtolower(preg_replace('/(?<!^)([A-Z]|\d+)/', $separator . '$1', $string));
+        return strtolower(
+            preg_replace( // precede any capital letters or numbers with the separator (except when the character starts the string)
+                "/(?<!^)([A-Z]|\\d+)/",
+                $separator . '$1',
+                preg_replace( // replace any non-word characters with the separator (e.g. for converting dash case to snake case)
+                    "/[^A-Za-z0-9]/",
+                    $separator,
+                    $string
+                )
+            )
+        );
+    }
+
+    /**
+     * @param array $data
+     * @param $case
+     * @return array
+     */
+    private function convertArrayKeys(array $data, $case) {
+
+        foreach ($data as $property => $value) {
+            $originalProperty = $property;
+            switch ($case) {
+                case StringCases::STUDLY_CAPS:
+                    $property = $this->toStudlyCaps($property);
+                    break;
+                case StringCases::CAMEL_CASE:
+                    $property = lcfirst($this->toStudlyCaps($property));
+                    break;
+                case StringCases::SNAKE_CASE:
+                    $property = $this->toSplitCase($property);
+                    break;
+                case StringCases::DASH_CASE:
+                    $property = $this->toSplitCase($property, "-");
+                    break;
+                default:
+                    if (strlen($case) == 1) {
+                        $property = $this->toSplitCase($property, $case);
+                    }
+                    break;
+            }
+            $data[$property] = $value;
+            if ($property != $originalProperty) {
+                unset($data[$originalProperty]);
+            }
+        }
+
+        return $data;
     }
 } 
---
+++ src/Model/StringCases.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Downsider\Clay\Model;
+
+/**
+ * StringCases
+ */
+class StringCases
+{
+
+    const STUDLY_CAPS = "StudlyCaps";
+
+    const CAMEL_CASE = "camelCase";
+
+    const SNAKE_CASE = "snake_case";
+
+    /**
+     * Alias of SNAKE_CASE
+     */
+    const UNDERSCORES = "snake_case";
+
+    const DASH_CASE = "dash-case";
+
+    /**
+     * Alias of DASH_CASE
+     */
+    const HYPHEN_CASE = "dash-case";
+
+}
